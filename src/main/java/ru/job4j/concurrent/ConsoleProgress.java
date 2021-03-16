982c17a4755434db2bf49231703bc1b745c63f13
vlad <vladislav.96@mail.ru>
2021-03-16 20:25:59 +0200

    Thread interrupt

---
+++ src/main/java/ru/job4j/concurrent/ConsoleProgress.java
@@ -0,0 +1,37 @@
+package ru.job4j.concurrent;
+
+import java.util.List;
+
+public class ConsoleProgress implements Runnable {
+    @Override
+    public void run() {
+        while (!Thread.currentThread().isInterrupted()) {
+            try {
+                System.out.print(String.format("\r Loading: %s", LoadingAnimation.nextSymbol()));
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                System.out.println("\r Loading complete");
+                System.exit(1);
+            }
+        }
+    }
+
+    private static class LoadingAnimation {
+        private static final List<String> symbols = List.of("-", "\\", "|", "/");
+        private static int currentSymbolIndex = 0;
+
+        public static String nextSymbol() {
+            if (currentSymbolIndex == symbols.size() - 1) {
+                currentSymbolIndex = 0;
+            }
+            return symbols.get(currentSymbolIndex++);
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread progress = new Thread(new ConsoleProgress());
+        progress.start();
+        Thread.sleep(1000); /* симулируем выполнение параллельной задачи в течение 1 секунды. */
+        progress.interrupt();
+    }
+}
