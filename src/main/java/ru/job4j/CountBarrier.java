33aab06031b3f52a36937187c068f2e69fb3e77f
vlad <vladislav.96@mail.ru>
2021-04-03 13:59:45 +0300

    Thread control through wait

---
+++ src/main/java/ru/job4j/CountBarrier.java
@@ -0,0 +1,32 @@
+package ru.job4j;
+
+public class CountBarrier {
+    private final Object monitor = this;
+
+    private final int total;
+
+    private int count = 0;
+
+    public CountBarrier(final int total) {
+        this.total = total;
+    }
+
+    public void count() {
+        synchronized (monitor) {
+            count++;
+            monitor.notifyAll();
+        }
+    }
+
+    public void await() {
+        synchronized (monitor) {
+            try {
+                while (count < total) {
+                    monitor.wait();
+                }
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+    }
+}
